693858d3f092b958b4cabd0dc775064674201fd5
Chunhao Zheng <chz062009@gmail.com>
2020-05-25 02:31:28 -0400

    0-4 exercise

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,10 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  str = title.split(" ");
+  for( var i = 0; i < str.length; i++){
+    str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+  }
+  return str.join(" ");
 }
 
 // Do not edit this line;
--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var boolCount = 0;
+  for(var i = 0; i < arr.length; i++){
+    if(typeof arr[i] == "boolean") 
+      boolCount++;
+  }
+  return boolCount;
 }
 
 // Do not edit this line;
